21613543f659ef08dde9eb6ad0d44ea562b1d7fa
Jayson Byrd <jayson_byrd_95@outlook.com>
2021-11-29 15:54:11 -0500

    Scaffolding

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import initial from './initialValues';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
@@ -22,6 +24,8 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoForm handleChange={this.handleChange}/>
+        <TodoList/>
       </div>
     );
   }
